dce33f9e3f6a278359e4452f5d49f9ec7a0f1221
Utku Erdoğanaras <utkuearasbusiness@gmail.com>
2021-03-29 19:01:12 +0300

    Add files via upload

---
+++ __callCounter.py
@@ -0,0 +1,5 @@
+def init():
+
+    global counter
+
+    counter = 0
---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .__callCounter import init
+
+init()
+
+print(1)
+
+from .squeezeNet.squeezeNetTool import *
+from .resNet.resNetTool import *
+from .mobileNet.mobileNetTool import *
